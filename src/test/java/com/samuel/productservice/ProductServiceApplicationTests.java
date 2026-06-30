8cbab96b75283c3e7dc663053558a6b653498bcd
Samuel Diniz Tenório <samueldiniztenorio.tech@gmail.com>
2026-06-29 22:04:30 -0300

    test: Extend ProductServiceApplicationTests to inherit from BaseContainersIntegrationTest

--- src/test/java/com/samuel/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/samuel/productservice/ProductServiceApplicationTests.java
@@ -3,8 +3,10 @@ package com.samuel.productservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.samuel.productservice.infrastructure.config.BaseContainersIntegrationTest;
+
 @SpringBootTest
-class ProductServiceApplicationTests {
+class ProductServiceApplicationTests extends BaseContainersIntegrationTest {
 
 	@Test
 	void contextLoads() {
